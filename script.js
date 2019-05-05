195334d1bf73376f8355212895503eec6d765630
alelopez98 <45163522+alelopez98@users.noreply.github.com>
2019-05-05 16:33:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+// Acordeon: Caracteristica JavaScript Nº1
+
+var acc = document.getElementsByClassName("acordeon");
+var i;
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+// Cuadro de busqueda: Caracteristica JavaScript Nº2
+// Descripción: El Cuadro de busqueda nos permitira que al clickar en una seccion veamos el contenido de dicha sección
+
+// Definimos la funcion y las variables
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    // con esto hacemos que al tener una seccion abierta, al abrir otra seccion la que estaba abierta se oculte y solo se vea la que acabamos de abrit
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    // ahora vamos a hacer que cuando pulsemos en otra seccion el color naranja deje de mostrarse en la seccion que se acaba de cerrar
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// : Caracteristica JavaScript Nº3
+
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
